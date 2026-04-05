e664a44e836682138a1088a3d7a4c0695c54ea57
IgorMartins0729 <igor.mrt02@gmail.com>
2026-04-05 18:42:28 -0300

    quarto commit

--- src/index.ts
+++ src/index.ts
@@ -1,90 +1,208 @@
 import * as readline from 'readline';
-import { Aeronave } from './models/Aeronave.js';
-import { Funcionario } from './models/Funcionario.js';
 import * as fs from 'fs';
 
+import { Aeronave } from './models/Aeronave';
+import { Funcionario } from './models/Funcionario';
+import { Peca } from './models/Peca';
+import { Etapa } from './models/Etapa'; 
+import { Teste } from './models/Teste';
+
 const leitor = readline.createInterface({ input: process.stdin, output: process.stdout });
-let frota: Array<Aeronave> = []
-let equipes: Array<Funcionario> = []
+let frota: Array<Aeronave> = [];
+let equipes: Array<Funcionario> = [];
 
 function exibirMenu(){
-    console.log("--Aerocode--");
-    console.log("1. Cadastar Aeronave:");
-    console.log("2. Cadastrar Funcionário:");
-    console.log("3. Sair");
+    console.log("\n-- Aerocode --");
+    console.log("1. Cadastrar Aeronave");
+    console.log("2. Cadastrar Funcionário");
+    console.log("3. Listar Cadastros");
+    console.log("4. Adicionar Peça a uma Aeronave");
+    console.log("5. Adicionar Etapa de Produção");
+    console.log("6. Adicionar Teste a uma Aeronave");
+    console.log("7. Sair");
 
     leitor.question("Escolha uma opção:  ", (opcaoEscolhida) => {
-            if (opcaoEscolhida == "1") {
-                console.log("Você escolheu cadastrar nave:")
-                cadastrarAeronave();
-            }
-            else if(opcaoEscolhida == "2"){
-                console.log("Você escolheu cadastrar Funcionário:")
-                cadastrarFuncionario();
-            }
-            else if(opcaoEscolhida == "3"){
-                return leitor.close()
-            }
-            else {
-                return exibirMenu();
-            }
-});
-  
+        if (opcaoEscolhida == "1") {
+            console.log("\nVocê escolheu cadastrar Aeronave:");
+            cadastrarAeronave();
+        }
+        else if(opcaoEscolhida == "2"){
+            console.log("\nVocê escolheu cadastrar Funcionário:");
+            cadastrarFuncionario();
+        }
+        else if(opcaoEscolhida == "3"){
+            console.log("\n-As FROTAS cadastradas são:");
+            console.log(JSON.stringify(frota, null, 2)); 
+            console.log("\n-As EQUIPES cadastradas são:");
+            console.log(JSON.stringify(equipes, null, 2));
+            console.log("----------------------------------\n");
+            console.log("----------------------------------\n");
+            exibirMenu();
+        }
+        else if(opcaoEscolhida == "4"){
+            console.log("\nVocê escolheu adicionar uma Peça:");
+            adicionarPeca();
+        }
+        else if(opcaoEscolhida == "5"){
+            console.log("\nVocê escolheu adicionar uma Etapa de Produção:");
+            adicionarEtapa();
+        }
+        else if(opcaoEscolhida == "6"){
+            console.log("\nVocê escolheu adicionar um Teste:");
+            adicionarTeste();
+        }
+        else if(opcaoEscolhida == "7"){
+            console.log("Finalizando..");
+            return leitor.close();
+        }
+        else {
+            console.log("Essa opção não existe.");
+            return exibirMenu();
+        }
+    });
 }
 
 function cadastrarAeronave(){
-    leitor.question("Qual o código?", (codigo_usuario) => {
-        leitor.question("Qual o modelo?", (modelo_usuario) => {
-            leitor.question("Qual o tipo de Aeronave?", (tipo_aeronave_usuario) => {
-                leitor.question("Qual a capacidade?", (capacidade_usuario) => {
-                    leitor.question("Qual o alcance?", (alcance_usuario) => {
+    leitor.question("Qual o código? ", (codigo_usuario) => {
+        leitor.question("Qual o modelo? ", (modelo_usuario) => {
+            leitor.question("Qual o tipo de Aeronave? ", (tipo_aeronave_usuario) => {
+                leitor.question("Qual a capacidade? ", (capacidade_usuario) => {
+                    leitor.question("Qual o alcance? ", (alcance_usuario) => {
                         const novaAeronave = new Aeronave(codigo_usuario, modelo_usuario, tipo_aeronave_usuario as any, Number(capacidade_usuario), Number(alcance_usuario));
-                        frota.push(novaAeronave)
-                        console.log("Aeronave foi cadastrada com sucesso");
+                        frota.push(novaAeronave);
+                        console.log("Aeronave cadastrada com sucesso");
                         salvarDados();
                         exibirMenu();
-                    })
-                })
-            }) 
-        })
-    })
+                    });
+                });
+            }); 
+        });
+    });
 }
 
 function cadastrarFuncionario(){
-    leitor.question("Qual o ID?", (id_usuario) => {
-        leitor.question("Qual nome?", (nome_usuario) => {
-            leitor.question("Qual o telefone", (telefone_usuario) => {
-                leitor.question("Qual o endereço?", (endereco_usuario) => {
-                    leitor.question("Qual o usuário?", (usuario_digitado) => {
-                        leitor.question("Qual a senha?", (senha_usuario) => {
-                            leitor.question("Qual a nivel de Permissão?", (permissao_usuario) => {
-                                const novoFuncionario = new Funcionario(id_usuario,nome_usuario,telefone_usuario,endereco_usuario,usuario_digitado,senha_usuario,permissao_usuario as any)
-                                console.log("Novo funcionário foi cadastrado com sucesso");
+    leitor.question("Qual o ID? ", (id_usuario) => {
+        leitor.question("Qual nome? ", (nome_usuario) => {
+            leitor.question("Qual o telefone? ", (telefone_usuario) => {
+                leitor.question("Qual o endereço? ", (endereco_usuario) => {
+                    leitor.question("Qual o usuário? ", (usuario_digitado) => {
+                        leitor.question("Qual a senha? ", (senha_usuario) => {
+                            leitor.question("Qual o nivel de Permissão? ", (permissao_usuario) => {
+                                const novoFuncionario = new Funcionario(id_usuario, nome_usuario, telefone_usuario, endereco_usuario, usuario_digitado, senha_usuario, permissao_usuario as any);
+                                console.log("Novo funcionário cadastrado com sucesso!");
                                 equipes.push(novoFuncionario);
                                 salvarDados();
                                 exibirMenu();
-                            })
-                        })
-                    })
-                })
-            })
-        })
-    })
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
+
+function adicionarPeca(){
+    leitor.question("Qual o código da aeronave que vai receber a peça? ", (codigo_digitado) => {
+        const procurandoCodigo = frota.find((aviao) => aviao.codigo === codigo_digitado);
+        
+        if (procurandoCodigo) {
+            leitor.question("Qual o ID da Peça? ", (id_digitado) =>{
+                leitor.question("Qual o tipo da Peça? ", (tipo_peca_digitada) => {
+                    leitor.question("Qual o fornecedor da peça? ", (fornecedor_digitado) => {
+                        leitor.question("Qual o status da peça? ", (status_peca_digitado) => {
+                            const novaPeca = new Peca(id_digitado, tipo_peca_digitada as any, fornecedor_digitado, status_peca_digitado as any);
+                            
+                            if (!procurandoCodigo.pecas) procurandoCodigo.pecas = [];
+                            
+                            procurandoCodigo.pecas.push(novaPeca);
+                            console.log("Peça adicionada com sucesso");
+                            salvarDados();
+                            exibirMenu();
+                        });
+                    });
+                });
+            });
+        }
+        else {
+            console.log("Avião não encontrado");
+            exibirMenu();
+        }
+    });
+}
+
+function adicionarEtapa(){
+    leitor.question("Qual o código da aeronave que receberá a etapa de produção? ", (codigo_digitado) => {
+        const procurandoCodigo = frota.find((aviao) => aviao.codigo === codigo_digitado);
+        
+        if (procurandoCodigo) {
+            leitor.question("Qual o nome da Etapa? ", (nome_digitado) =>{
+                leitor.question("Qual o prazo da Etapa? ", (prazo_digitado) => {
+                    leitor.question("Qual o status da Etapa? ", (status_digitado) => {
+                        
+                        const novaEtapa = new Etapa(nome_digitado, prazo_digitado, status_digitado as any);
+                        
+                        if (!procurandoCodigo.etapas) procurandoCodigo.etapas = [];
+                        
+                        procurandoCodigo.etapas.push(novaEtapa);
+                        console.log("Etapa de produção adicionada com sucesso");
+                        salvarDados();
+                        exibirMenu();
+                    });
+                });
+            });
+        }
+        else {
+            console.log("Avião não encontrado");
+            exibirMenu();
+        }
+    });
+}
+
+function adicionarTeste(){
+    leitor.question("Qual o código da aeronave que receberá o teste? ", (codigo_digitado) => {
+        const procurandoCodigo = frota.find((aviao) => aviao.codigo === codigo_digitado);
+        
+        if (procurandoCodigo) {
+            leitor.question("Qual o tipo do Teste? ", (tipo_digitado) =>{
+                leitor.question("Qual o resultado do Teste? ", (resultado_digitado) => {
+                    
+                    const novoTeste = new Teste(tipo_digitado as any, resultado_digitado as any);
+                    
+                    if (!procurandoCodigo.testes) procurandoCodigo.testes = [];
+                    
+                    procurandoCodigo.testes.push(novoTeste);
+                    console.log("Teste adicionado com sucesso");
+                    salvarDados();
+                    exibirMenu();
+                });
+            });
+        }
+        else {
+            console.log("Avião não encontrado");
+            exibirMenu();
+        }
+    });
 }
 
 function carregarDados(){
     if (fs.existsSync('frota.json')){
-        
+        const textoFrota = fs.readFileSync('frota.json','utf-8');
+        frota = JSON.parse(textoFrota);
+    }
+    if (fs.existsSync('equipes.json')){
+        const textoEquipes = fs.readFileSync('equipes.json', 'utf-8');
+        equipes = JSON.parse(textoEquipes);
     }
 }
 
 function salvarDados(){
-    const dadosFrota = JSON.stringify(frota, null, 2)
-    fs.writeFileSync('frota.json',dadosFrota);
-    const dadosEquipes = JSON.stringify(equipes, null, 2)
+    const dadosFrota = JSON.stringify(frota, null, 2);
+    fs.writeFileSync('frota.json', dadosFrota);
+    const dadosEquipes = JSON.stringify(equipes, null, 2);
     fs.writeFileSync('equipes.json', dadosEquipes);
 }
 
-
-
+// Embraer
+carregarDados();
 exibirMenu();
